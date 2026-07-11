e8b274d1a706be39426caaf160e979498e909b42
iki1uc <entertrainer@web.de>
2026-07-11 19:02:06 +0200

    Create NAME.js

---
+++ TOOL/NAME.js
@@ -0,0 +1,10 @@
+/* NAME.js – Systemnamen für TOOL / RESPO / ORBIT */
+
+export function NAME(file) {
+    return {
+        core: "iki1uc",
+        modul: file,
+        signatur: "IKI1UC-" + file.toUpperCase(),
+        respo: "60000-" + file.length + "-" + file.charCodeAt(0)
+    };
+}
