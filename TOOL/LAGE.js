c118d1fc4e8398c1665e5c10a0e64553a13f1904
iki1uc <entertrainer@web.de>
2026-07-11 19:02:47 +0200

    Update LAGE.js

--- TOOL/LAGE.js
+++ TOOL/LAGE.js
@@ -1,1 +1,12 @@
+/* LAGE.js – Lagezuordnung für TOOL / ORBIT / ARBIT */
+
+export function LAGE(Phi, phi, phi2, phiinfty) {
+    return {
+        x: Phi * 10,
+        y: phi * 10,
+        z: phiinfty * 10,
+        stabil: phi2,
+        lage_id: "L-" + Phi + "-" + phi + "-" + phi2 + "-" + phiinfty
+    };
+}
 
