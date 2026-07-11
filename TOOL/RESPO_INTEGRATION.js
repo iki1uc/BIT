f804bbe1986f1086ea813e8281be96076ab8c7cc
iki1uc <entertrainer@web.de>
2026-07-11 18:52:01 +0200

    Create RESPO_INTEGRATION.js

---
+++ TOOL/RESPO_INTEGRATION.js
@@ -0,0 +1,9 @@
+export function RESPO_INTEGRATION(meta, orbit, arbit) {
+
+    return {
+        id: "60000-" + orbit.axis.Phi + "-" + orbit.axis.phi,
+        status: meta.original ? "IKI1UC-OK" : "FEHLER",
+        konstellation: arbit.konstellation,
+        austausch: arbit.austausch
+    };
+}
