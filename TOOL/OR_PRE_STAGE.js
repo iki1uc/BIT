2b6d93a8b7b298eacb71a3cf1bc02be8dffe8c1e
iki1uc <entertrainer@web.de>
2026-07-11 19:05:49 +0200

    Update OR_PRE_STAGE.js

--- TOOL/OR_PRE_STAGE.js
+++ TOOL/OR_PRE_STAGE.js
@@ -1,11 +1,8 @@
-/* OR_PRE_STAGE – Vorstufe für alle iki1uc-Systemdateien */
-
-export function OR_PRE_STAGE(file) {
-
+export function OR_PRE_STAGE(file){
     return {
-        original: file.includes("iki1uc"),
-        editable: true,
-        signature: "IKI1UC-CORE",
+        original: true,
+        signature: "IKI1UC-" + file.toUpperCase(),
+        file,
         arbit_ready: true,
         orbit_ready: true,
         respo_ready: true
