d745970763cac5518b92c61ec587230308671cb0
iki1uc <entertrainer@web.de>
2026-07-11 19:06:08 +0200

    Update ARBIT.js

--- TOOL/ARBIT.js
+++ TOOL/ARBIT.js
@@ -1,7 +1,5 @@
-export function ARBIT(Phi, phi, phi2, phiinfty) {
-
-    const stabil = (phi2 >= 0 && phi >= 0);
-
+export function ARBIT(Phi, phi, phi2, phiinfty){
+    const stabil = (phi >= 0 && phi2 >= 0);
     return {
         stabil,
         austausch: stabil ? "+" : "-",
